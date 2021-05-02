5a4e80751d11acbd24109e1fed0f781ce6f84406
Tiktokcity <danhskipper18@gmail.com>
2021-05-02 16:51:35 +0700

    [DanhPV] fix sql injection room detail model

--- Back-end/Models/RoomDetailModel.js
+++ Back-end/Models/RoomDetailModel.js
@@ -12,7 +12,7 @@ var RoomDetails = function(roomDetail) {
     this.status = roomDetail.status;
 }
 
-RoomDetails.create_new_room_details_two_users = (body, room, callback) => {
+RoomDetails.create_new_room_details_two_users = async(body, room, callback) => {
     if (!body.roomNameFrom) {
         body.roomNameFrom = '';
     }
@@ -26,35 +26,82 @@ RoomDetails.create_new_room_details_two_users = (body, room, callback) => {
         body.roomAvatarTo = '';
     }
     var defaultStatus = 1;
-    connection.then(() => {
-        return sql.query("Insert into RoomDetails(roomName, roomAvatar, roomId, userId, status) values(N'" +
-            body.roomNameTo + "', '" + body.roomAvatarTo + "', '" +
-            room.roomId + "', '" + body.userFromId + "', " + defaultStatus + "), (N'" +
-            body.roomNameFrom + "', '" + body.roomAvatarFrom + "', '" +
-            room.roomId + "', '" + body.userToId + "', " + defaultStatus + ")");
-    }).then(result => {
-        callback(null, result);
-    }).catch(error => {
-        callback(error, null);
+    await connection.getConnection(async(error, result) => {
+        if (error) {
+            callback("Connection to mssql server failed!", null);
+        }
+        if (result) {
+            await result.request()
+                .input("roomNameTo", sql.NVarChar, body.roomNameTo)
+                .input("roomAvatarTo", sql.NVarChar, body.roomAvatarTo)
+                .input("roomId", sql.Int, room.roomId)
+                .input("userFromId", sql.VarChar, body.userFromId)
+                .input("status", sql.Int, defaultStatus)
+                .input("roomNameFrom", sql.NVarChar, body.roomNameFrom)
+                .input("roomAvatarFrom", sql.NVarChar, body.roomAvatarFrom)
+                .input("userToId", sql.VarChar, body.userToId)
+                .query("Insert into RoomDetails(roomName, roomAvatar, roomId, userId, status) values(@roomNameTo, " +
+                    " @roomAvatarTo , @roomId, @userFromId, @status), (@roomNameFrom, @roomAvatarFrom, @roomId, @userToId, @status)").then(result => {
+                    callback(null, result);
+                }).catch(error => {
+                    callback(error, null);
+                })
+        }
+    }).finally(() => {
+        connection.closeConnection();
     })
+
 }
 
-RoomDetails.create_new_room_details_multi_users = (listCart, body, room, callback) => {
+RoomDetails.create_new_room_details_multi_users = async(listCart, body, room, callback) => {
     if (!body.roomAvatar) {
         body.roomAvatar = '';
     }
     if (body.roomName) {
         var defaultStatus = 1;
-        connection.then(() => {
-            for (let i = 0; i < listCart.length; i++) {
-                sql.query("Insert into RoomDetails(roomName, roomAvatar, roomId, userId, status) values(N'" +
-                    body.roomName + "', '" + body.roomAvatar + "', '" +
-                    room.roomId + "', '" + listCart[i].userId + "', " + defaultStatus + ")");
+        var checkStatus = false;
+        await connection.getConnection(async(error, result) => {
+            if (error) {
+                callback("Connection to mssql server failed!", null);
             }
-        }).then(result => {
-            callback(null, "Create new room multi users successfull.");
-        }).catch(error => {
-            callback(error, null);
+            if (result) {
+
+                /* Using Transaction in query */
+
+                const transaction = new sql.Transaction(result);
+                transaction.begin(async(err) => {
+                    if (err) {
+                        callback(err, null);
+                    }
+                    for (let i = 0; i < listCart.length; i++) {
+                        await result.request()
+                            .input("roomName", sql.NVarChar, body.roomName)
+                            .input("roomAvatar", sql.NVarChar, body.roomAvatar)
+                            .input("roomId", sql.Int, room.roomId)
+                            .input("userId", sql.VarChar, listCart[i].userId)
+                            .input("status", sql.Int, defaultStatus)
+                            .query("Insert into RoomDetails(roomName, roomAvatar, roomId, userId, status) values(@roomName, " +
+                                " @roomAvatar, @roomId , @userId , @status)").then(result => {
+                                checkStatus = true;
+                            }).catch(async(error) => {
+                                await transaction.rollback(() => {
+                                    callback(error, null);
+                                });
+                                checkStatus = false;
+                            })
+                    }
+                    await transaction.commit(() => {
+                        callback(null, "Create new room details with multi users successfully!");
+                    })
+                })
+            }
+        }).finally(() => {
+            if (checkStatus) {
+                callback(null, "Create new room details with multi users successfully!");
+            } else {
+                callback("Create new room details with multi users failed!", null);
+            }
+            connection.closeConnection();
         })
     } else {
         callback("Require input room name!", null);
@@ -62,73 +109,147 @@ RoomDetails.create_new_room_details_multi_users = (listCart, body, room, callbac
 
 }
 
-RoomDetails.getRoomDetailsByUserId = (userId, callback) => {
+RoomDetails.getRoomDetailsByUserId = async(userId, callback) => {
     var defaultStatus = 1;
-    connection.then(() => {
-        return sql.query("SELECT roomDetails.*, room.sender, room.content, room.fullName FROM (SELECT roomDetailId, roomName, roomAvatar, roomId FROM RoomDetails WHERE userId = '" + userId + "') roomDetails " +
-            " LEFT JOIN ( " +
-            " SELECT roomDetails.roomDetailId, roomDetails.roomName, roomDetails.roomAvatar, roomDetails.roomId, messages.content, messages.sender, users.fullName     FROM RoomDetails roomDetails JOIN (SELECT roomDetails.roomId, max(createdAt) as 'createdAtMess' FROM RoomDetails roomDetails JOIN " +
-            " (SELECT roomDetailID, max(createdAt) as 'createdAt' FROM Messages WHERE " +
-            " roomDetailId in (SELECT roomDetailId FROM RoomDetails WHERE roomId in " +
-            " (SELECT roomId FROM RoomDetails WHERE userId = '" + userId + "' and status = " + defaultStatus + ")) GROUP BY roomDetailId) cs ON roomDetails.roomDetailId = cs.roomDetailId " +
-            " GROUP BY roomDetails.roomId) " +
-            " custom ON roomDetails.roomId = custom.roomId  " +
-            " JOIN Users users ON roomDetails.userId = users.userId" +
-            " JOIN Messages messages ON messages.roomDetailId = roomDetails.roomDetailId and custom.createdAtMess = messages.createdAt WHERE " +
-            " roomDetails.status = " + defaultStatus + " and messages.status = " + defaultStatus + ") room " +
-            " ON roomDetails.roomId = room.roomId");
-    }).then(result => {
-        callback(null, result.recordsets[0]);
-    }).catch(error => {
-        callback(error, null);
-    });
-}
 
-RoomDetails.getRoomDetailsByRoomDetailId = (roomDetailId, callback) => {
-    var defaultStatus = 1;
-    connection.then(() => {
-        return sql.query("SELECT * FROM RoomDetails WHERE roomId in (select roomId from RoomDetails where roomDetailId = '" + roomDetailId + "' and status = " + defaultStatus + ") and status = " + defaultStatus);
-    }).then(result => {
-        callback(null, result.recordsets[0]);
-    }).catch(error => {
-        callback(error, null);
+    await connection.getConnection(async(error, result) => {
+        if (error) {
+            callback("Connection to mssql server failed!", null);
+        }
+        if (result) {
+            await result.request()
+                .input("userId", sql.VarChar, userId)
+                .input("status", sql.Int, defaultStatus)
+                .query("SELECT roomDetails.*, room.sender, room.content, room.fullName FROM (SELECT roomDetailId, roomName, roomAvatar, roomId FROM RoomDetails WHERE userId = @userId) roomDetails " +
+                    " LEFT JOIN ( " +
+                    " SELECT roomDetails.roomDetailId, roomDetails.roomName, roomDetails.roomAvatar, roomDetails.roomId, messages.content, messages.sender, users.fullName     FROM RoomDetails roomDetails JOIN (SELECT roomDetails.roomId, max(createdAt) as 'createdAtMess' FROM RoomDetails roomDetails JOIN " +
+                    " (SELECT roomDetailID, max(createdAt) as 'createdAt' FROM Messages WHERE " +
+                    " roomDetailId in (SELECT roomDetailId FROM RoomDetails WHERE roomId in " +
+                    " (SELECT roomId FROM RoomDetails WHERE userId = @userId and status = @status)) GROUP BY roomDetailId) cs ON roomDetails.roomDetailId = cs.roomDetailId " +
+                    " GROUP BY roomDetails.roomId) " +
+                    " custom ON roomDetails.roomId = custom.roomId  " +
+                    " JOIN Users users ON roomDetails.userId = users.userId" +
+                    " JOIN Messages messages ON messages.roomDetailId = roomDetails.roomDetailId and custom.createdAtMess = messages.createdAt WHERE " +
+                    " roomDetails.status = @status and messages.status = @status) room " +
+                    " ON roomDetails.roomId = room.roomId").then(result => {
+                    callback(null, result.recordsets[0]);
+                }).catch(error => {
+                    callback(error, null);
+                });
+        }
+    }).finally(() => {
+        connection.closeConnection();
     })
+
 }
 
-RoomDetails.getRoomDetailByUserIdAndRoomId = (sender, roomId, callback) => {
+RoomDetails.getRoomDetailsByRoomDetailId = async(roomDetailId, callback) => {
     var defaultStatus = 1;
-    connection.then(() => {
-        return sql.query("SELECT * FROM RoomDetails WHERE userId = '" + sender + "' and roomId = " + roomId + " and status = " + defaultStatus);
-    }).then(result => {
-        callback(null, result.recordsets[0]);
-    }).catch(error => {
-        callback(error, null);
-    });
+
+    await connection.getConnection(async(error, result) => {
+        if (error) {
+            callback("Connection to mssql server failed!", null);
+        }
+        if (result) {
+            await result.request()
+                .input("roomDetailId", sql.Int, roomDetailId)
+                .input("status", sql.Int, defaultStatus)
+                .query("SELECT * FROM RoomDetails WHERE roomId in (select roomId from RoomDetails where roomDetailId = @roomDetailId and status = @status) and status = @status ").then(result => {
+                    callback(null, result.recordsets[0]);
+                }).catch(error => {
+                    callback(error, null);
+                })
+        }
+    }).finally(() => {
+        connection.closeConnection();
+    })
+
 }
 
-RoomDetails.getRoomDetailsBetweenUsers = (userFromId, userToId, callback) => {
+RoomDetails.getRoomDetailByUserIdAndRoomId = async(sender, roomId, callback) => {
+    var defaultStatus = 1;
+
+    await connection.getConnection(async(error, result) => {
+        if (error) {
+            callback("Connection to mssql server failed!", null);
+        }
+        if (result) {
+            await result.request()
+                .input("userId", sql.VarChar, sender)
+                .input("roomId", sql.Int, roomId)
+                .input("status", sql.Int, defaultStatus)
+                .query("SELECT * FROM RoomDetails WHERE userId = @userId and roomId = @roomId and status = @status").then(result => {
+                    callback(null, result.recordsets[0]);
+                }).catch(error => {
+                    callback(error, null);
+                });
+        }
+    }).finally(() => {
+        connection.closeConnection();
+    })
+
+}
+
+RoomDetails.getRoomDetailsBetweenUsers = async(userFromId, userToId, callback) => {
     var defaultStatus = 1;
     var defaultCount = 2;
-    connection.then(() => {
-        return sql.query("SELECT * FROM RoomDetails WHERE roomId IN (SELECT custom.roomId FROM RoomDetails roomDetail RIGHT JOIN  ( SELECT groupUserFrom.roomId FROM (SELECT roomId FROM RoomDetails WHERE userId = '" + userFromId + "') groupUserFrom  JOIN (SELECT roomId FROM RoomDetails WHERE userId = '" + userToId + "') groupUserTo " +
-            " ON groupUserFrom.roomId = groupUserTo.roomId) custom ON roomDetail.roomId = custom.roomId GROUP BY custom.roomId HAVING COUNT(*) = " + defaultCount + ") and userId = '" + userFromId + "' and status = " + defaultStatus);
-    }).then(result => {
-        callback(null, result.recordsets[0]);
-    }).catch(error => {
-        callback(error, null);
+
+    await connection.getConnection(async(error, result) => {
+        if (error) {
+            callback("Connection to mssql server failed!", null);
+        }
+        if (result) {
+            await result.request()
+                .input("userFromId", sql.VarChar, userFromId)
+                .input("userToId", sql.VarChar, userToId)
+                .input("status", sql.Int, defaultStatus)
+                .input("count", sql.Int, defaultCount)
+                .query("SELECT * FROM RoomDetails WHERE roomId IN (SELECT custom.roomId FROM RoomDetails roomDetail RIGHT JOIN  " +
+                    "( SELECT groupUserFrom.roomId FROM (SELECT roomId FROM RoomDetails WHERE userId = @userFromId) " +
+                    "groupUserFrom  JOIN (SELECT roomId FROM RoomDetails WHERE userId = @userToId) groupUserTo " +
+                    " ON groupUserFrom.roomId = groupUserTo.roomId) custom ON roomDetail.roomId = custom.roomId " +
+                    " GROUP BY custom.roomId HAVING COUNT(*) = @count) and " +
+                    " userId = @userFromId and status = @status ").then(result => {
+                    callback(null, result.recordsets[0]);
+                }).catch(error => {
+                    callback(error, null);
+                })
+        }
+    }).finally(() => {
+        connection.closeConnection();
     })
+
 }
 
-RoomDetails.checkExistGroupBetweenTwoUsers = (userFromId, userToId, callback) => {
-    var defaultStatus = 1;
-    connection.then(() => {
-        return sql.query("SELECT COUNT(*) as 'Count' FROM RoomDetails roomDetail RIGHT JOIN  ( SELECT groupUserFrom.roomId FROM (SELECT roomId FROM RoomDetails WHERE userId = '" + userFromId + "' and status = " + defaultStatus + ") groupUserFrom  JOIN (SELECT roomId FROM RoomDetails WHERE userId = '" + userToId + "' and status = " + defaultStatus + ") groupUserTo " +
-            " ON groupUserFrom.roomId = groupUserTo.roomId) custom ON roomDetail.roomId = custom.roomId GROUP BY custom.roomId HAVING COUNT(*)= 2");
-    }).then(result => {
-        callback(null, result.recordsets[0]);
-    }).catch(error => {
-        callback(error, null);
+RoomDetails.checkExistGroupBetweenTwoUsers = async(userFromId, userToId, callback) => {
+
+    await connection.getConnection(async(error, result) => {
+        if (error) {
+            callback("Connection to mssql server failed!", null);
+        }
+        if (result) {
+            let defaultStatus = 1;
+            let defaultCount = 2;
+            await result.request()
+                .input("userFromId", sql.VarChar, userFromId)
+                .input("userToId", sql.VarChar, userToId)
+                .input("status", sql.Int, defaultStatus)
+                .input("count", sql.Int, defaultCount)
+                .query("SELECT COUNT(*) as 'Count' FROM RoomDetails roomDetail RIGHT JOIN " +
+                    " ( SELECT groupUserFrom.roomId FROM (SELECT roomId FROM RoomDetails " +
+                    "WHERE userId = @userFromId and status = @status) groupUserFrom  JOIN " +
+                    "(SELECT roomId FROM RoomDetails WHERE userId = @userToId and status = @status) groupUserTo " +
+                    " ON groupUserFrom.roomId = groupUserTo.roomId) custom ON roomDetail.roomId = custom.roomId " +
+                    "GROUP BY custom.roomId HAVING COUNT(*)= @count").then(result => {
+                    callback(null, result.recordsets[0]);
+                }).catch(error => {
+                    callback(error, null);
+                })
+        }
+    }).finally(() => {
+        connection.closeConnection();
     })
+
 }
 
 module.exports = RoomDetails;
